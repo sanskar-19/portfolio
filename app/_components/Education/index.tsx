783b4cf416608d6fabbf9a82e696720117f22ffd
Sanskar Agrawal <sanskaragrawal@Sanskars-MacBook-Air.local>
2023-07-08 17:26:00 +0530

    removing firm

--- app/_components/Education/index.tsx
+++ app/_components/Education/index.tsx
@@ -1,8 +1,9 @@
 import { educationList } from "@/utils/education";
 import EducationItem from "./EducationItem";
+import { RefObject } from "react";
 
-const Education = () => {
-    return (<section className="container-wrapper h-max min-h-[calc(100vh-5rem)] justify-center lg:justify-start sm:h-full snap-start flex flex-col py-20 gap-y-8 lg:gap-y-0" id="education" >
+const Education = ({ educationRef }: { educationRef: RefObject<HTMLDivElement> }) => {
+    return (<section className="container-wrapper h-max min-h-[calc(100vh-5rem)] justify-center lg:justify-start sm:h-full snap-start flex flex-col py-20 gap-y-8 lg:gap-y-0" id="education" ref={educationRef}>
         <h1 className="text-[2rem] lg:text-left sm:text-[4rem] font-bold"><span className="text-greenPrimary">Edu</span>cation</h1>
         <section className="max-w-7xl mx-auto w-full flex flex-col gap-y-14">
             <section className="flex flex-col gap-y-14 sm:border-l-2 sm:p-8">
--- app/_components/Popup/Contact.tsx
+++ app/_components/Popup/Contact.tsx
@@ -7,11 +7,12 @@ const Contact = () => {
     return (<main className="flex mx-auto">
         <form className="mx-auto flex-grow flex flex-col gap-y-8">
             <h2 className="text-white font-semibold text-4xl">Let&apos;s <span className="font-thin">connect</span></h2>
-            <Input name="first_name" placeholder="John" type="text" register={register} extraClass="w-full " />
+            <h4 className="text-xl">Drop an email to <span className="text-greenPrimary">dummy@dummy.com</span></h4>
+            {/* <Input name="first_name" placeholder="John" type="text" register={register} extraClass="w-full " />
             <Input name="last_name" placeholder="Doe" type="text" register={register} extraClass="w-full " />
             <Input name="email" placeholder="johndoe@example.com" type="email" register={register} extraClass="w-full " />
             <textarea placeholder="Hey Sanskar, greetings. My business name is ABC..." {...register("message")} className="outline-none focus:outline-none border border-white bg-transparent py-2 px-4 rounded-md w-full" rows={4} />
-            <Button styleType="primary" label={"Send message"} extraClass={"text-white green-shadow"} disabled={!isValid} />
+            <Button styleType="primary" label={"Send message"} extraClass={"text-white green-shadow"} disabled={!isValid} /> */}
         </form>
     </main>);
 }
